b3c52dc818032afaf7b4e4382920d527a868493a
ehdnd_desktop <taeungjang@naver.com>
2024-12-14 22:22:24 +0900

    REF grade scraper

--- workflows/grade_scraper.py
+++ workflows/grade_scraper.py
@@ -3,6 +3,24 @@ from utils.slack_notifier import SlackNotifier
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def make_message(spans: list):
+    prev = None
+    message = ["=" * 10]
+    for span in spans[16:]:
+        text = span.text.strip()
+        if text == prev or text == "조회":
+            continue
+        prev = text
+        message.append(text)
+        if text.isdigit() and len(text) >= 4:
+            message.append("=" * 10)
+
+    return message
 
 
 def grade_scraper():
@@ -22,25 +40,22 @@ def grade_scraper():
 
         try:
             scraper.login(page, url)
-            content = scraper.fetch_page_content(
-                page,
+            selectors = [
                 'li.c_node > a.depth1.c_nodeA:has-text("학사관리")',
-                "isolatedWorkArea",
+                'li.c_node.active > div.depth2_w > ul > li > a.c_nodeA:has-text("성적/졸업")',
+            ]
+            content = scraper.fetch_page_content(
+                page, selectors=selectors, isPrev=False
             )
 
             soup = BeautifulSoup(content, "html.parser")
             tbody = soup.find("tbody", id="WD0188-contentTBody")
             spans = tbody.find_all("span")
 
-            messages = ["==========="]
-            for span in spans[16:]:
-                text = span.text.strip()
-                if text.isdigit() and len(text) >= 4:
-                    messages.append("===========")
-                messages.append(text)
-
+            messages = make_message(spans)
             notifier.send_message("<!channel> 성적을 불러왔어요")
             notifier.send_message("\n".join(messages))
+
         except Exception as e:
             notifier.send_message(f"오류 발생: {e}")
         finally:
